cd19d2231031c28e66ac82cc6bf556da08695071
hamza waqar <hamza.waqar@nebrascorp.com>
2024-01-10 13:49:55 +0500

    Over View Topics

--- overView.js
+++ overView.js
@@ -1,1 +1,13 @@
 //++++ inst this section we will learn soo much things
+
+//1.  Core concept : component , props ,Jsx
+
+//2. Creating and reusing component
+
+//3. Rendering lists
+
+//4. Condition rendering
+
+//5. start writing code on your own!
+
+//6. by creatin pizza  Cart project
