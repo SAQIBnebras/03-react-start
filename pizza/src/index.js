b9ee28927fa4b449c98d67d29434a6c9bfed4609
hamza waqar <hamza.waqar@nebrascorp.com>
2024-01-10 14:22:22 +0500

    create 1st Heading

---
+++ pizza/src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+function App(){
+    return <div>
+        <h1>Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad </h1>
+        <h1>Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad </h1>
+        <h1>Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad </h1>
+        <h1>Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad </h1>
+        <h1>Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad </h1>
+        <h1>Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad </h1>
+        <h1>Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad </h1>
+        <h1>Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad </h1>
+        <h1>Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad </h1>
+        <h1>Pakistan Zindabad Pakistan Zindabad Pakistan Zindabad </h1>
+    </div>
+}
+
+//react 18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    // <App />
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
